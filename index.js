b8666bbd46162321b4e9d6498b97bb74f2d90a91
courtneyhutchin47@gmail.com <courtneyhutchin47@gmail.com>
2020-03-16 13:44:56 -0700

    Added jumping bubble animation

--- index.js
+++ index.js
@@ -3,12 +3,42 @@ window.addEventListener("load", () => {
     const sounds = document.querySelectorAll(".sound");
     // Grab the divs that hold the sounds
     const pads = document.querySelectorAll(".pads div");
+    // Grab the "visual" div
+    const visual = document.querySelector(".visual");
+    // Create an array to hold the colors
+    const colors = [
+        "#60d394",
+        "#d36060",
+        "#c060d3",
+        "#d3d160",
+        "#606bd3",
+        "#60c2d3"
+    ];
 
     // Hold each sound at the specific index
     pads.forEach((pad, index) => {
         pad.addEventListener("click", function () {
-            // Play the correct per [index] sound
+            // Allows the user to click the same button multiple times
+            sounds[index].currentTime = 0;
+            // Play the correct sound per [index]
             sounds[index].play();
+
+            createBubbles(index);
         })
     })
+
+    // Create a function that makes bubbles appear
+    const createBubbles = index => {
+        const bubble = document.createElement("div");
+        // Attach "bubble" to the "visual" div
+        visual.appendChild(bubble);
+        // Change the color
+        bubble.style.backgroundColor = colors[index];
+        bubble.style.animation = "jump 1s ease";
+
+        // Remvove the bubble after it goes off the screen
+        bubble.addEventListener("animationend", function () {
+            visual.removeChild(this);
+        })
+    }
 })
